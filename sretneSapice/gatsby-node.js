f464d721238ef19a7bb080ed348d6683356227f0
leamladenic <lea.mladenic3@gmail.com>
2020-01-09 00:52:28 +0100

    df

--- sretneSapice/gatsby-node.js
+++ sretneSapice/gatsby-node.js
@@ -26,7 +26,7 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     })
   })
-}}
+}
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const result = await graphql(`
