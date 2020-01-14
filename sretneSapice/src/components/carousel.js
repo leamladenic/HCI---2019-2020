e8b993fb3ab8708d071cf4cdcb765dc360446fa1
leamladenic <lea.mladenic3@gmail.com>
2020-01-14 18:13:33 +0100

    res

--- sretneSapice/src/components/carousel.js
+++ sretneSapice/src/components/carousel.js
@@ -5,6 +5,7 @@ import "../components/index.min.css"
 import { Link } from "gatsby"
 import { IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io"
 import Image from "gatsby-image"
+import Media from "react-media"
 
 export const myProducts = graphql`
   {
@@ -31,68 +32,140 @@ export const myProducts = graphql`
 export default function Carousel() {
   const [activeItemIndex, setActiveItemIndex] = useState(0)
   const chevronWidth = 40
+  var numberCard = 4
   return (
     <StaticQuery
       query={myProducts}
       render={data => {
         return (
           <div className="main-carousel" style={{ padding: `50px` }}>
-            <ItemsCarousel
-              infiniteLoop={true}
-              requestToChangeActive={setActiveItemIndex}
-              activeItemIndex={activeItemIndex}
-              numberOfCards={4}
-              gutter={20}
-              leftChevron={
-                <button className="arrow-carousel">
-                  <IoMdArrowDropleft />
-                </button>
-              }
-              rightChevron={
-                <button className="arrow-carousel">
-                  <IoMdArrowDropright />
-                </button>
-              }
-              outsideChevron
-              chevronWidth={chevronWidth}
-            >
-              {data.products.edges.map(({ node: product }) => {
-                return (
-                  <div key={product.id}>
-                    <div style={{ height: 320, background: "white" }}>
-                      <div className="carousel-card">
-                        <div className="carousel-card-image">
-                          <Image
-                            fluid={product.image.fluid}
-                            className="carousel-card-image-image"
-                          />
-                        </div>
-                        <div className="card-bottom">
-                          {" "}
-                          <div className="carousel-card-info">
-                            <p className="price-label-carousel">CIJENA:</p>
-                            <p className="price-carousel">
-                              {product.price + "kn"}
-                            </p>
+            <Media queries={{ small: { maxWidth: 700 } }}>
+              {matches =>
+                matches.small ? (
+                  <ItemsCarousel
+                    infiniteLoop={true}
+                    requestToChangeActive={setActiveItemIndex}
+                    activeItemIndex={activeItemIndex}
+                    numberOfCards={1}
+                    gutter={20}
+                    leftChevron={
+                      <button className="arrow-carousel">
+                        <IoMdArrowDropleft />
+                      </button>
+                    }
+                    rightChevron={
+                      <button className="arrow-carousel">
+                        <IoMdArrowDropright />
+                      </button>
+                    }
+                    outsideChevron
+                    chevronWidth={chevronWidth}
+                  >
+                    {data.products.edges.map(({ node: product }) => {
+                      return (
+                        <div key={product.id}>
+                          <div style={{ height: 320, background: "white" }}>
+                            <div className="carousel-card">
+                              <div className="carousel-card-image">
+                                <Image
+                                  fluid={product.image.fluid}
+                                  className="carousel-card-image-image"
+                                />
+                              </div>
+                              <div className="card-bottom">
+                                {" "}
+                                <div className="carousel-card-info">
+                                  <p className="price-label-carousel">
+                                    CIJENA:
+                                  </p>
+                                  <p className="price-carousel">
+                                    {product.price + "kn"}
+                                  </p>
+                                </div>
+                                <div className="carousel-card-button">
+                                  <Link
+                                    to={`/${product.slug}`}
+                                    product={product}
+                                    state={{
+                                      modal: true,
+                                    }}
+                                  >
+                                    <button className="buy-carousel">
+                                      Pogledaj
+                                    </button>
+                                  </Link>
+                                </div>
+                              </div>
+                            </div>
                           </div>
-                          <div className="carousel-card-button">
-                            <Link
-                              to={`/${product.slug}`}
-                              product={product}
-                              state={{
-                                modal: true,
-                              }}
-                            >
-                              <button className="buy-carousel">Pogledaj</button>
-                            </Link>
+                        </div>
+                      )
+                    })}
+                  </ItemsCarousel>
+                ) : (
+                  <ItemsCarousel
+                    infiniteLoop={true}
+                    requestToChangeActive={setActiveItemIndex}
+                    activeItemIndex={activeItemIndex}
+                    numberOfCards={4}
+                    gutter={20}
+                    leftChevron={
+                      <button className="arrow-carousel">
+                        <IoMdArrowDropleft />
+                      </button>
+                    }
+                    rightChevron={
+                      <button className="arrow-carousel">
+                        <IoMdArrowDropright />
+                      </button>
+                    }
+                    outsideChevron
+                    chevronWidth={chevronWidth}
+                  >
+                    {data.products.edges.map(({ node: product }) => {
+                      return (
+                        <div key={product.id}>
+                          <div style={{ height: 320, background: "white" }}>
+                            <div className="carousel-card">
+                              <div className="carousel-card-image">
+                                <Image
+                                  fluid={product.image.fluid}
+                                  className="carousel-card-image-image"
+                                />
+                              </div>
+                              <div className="card-bottom">
+                                {" "}
+                                <div className="carousel-card-info">
+                                  <p className="price-label-carousel">
+                                    CIJENA:
+                                  </p>
+                                  <p className="price-carousel">
+                                    {product.price + "kn"}
+                                  </p>
+                                </div>
+                                <div className="carousel-card-button">
+                                  <Link
+                                    to={`/${product.slug}`}
+                                    product={product}
+                                    state={{
+                                      modal: true,
+                                    }}
+                                  >
+                                    <button className="buy-carousel">
+                                      Pogledaj
+                                    </button>
+                                  </Link>
+                                </div>
+                              </div>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                  </div>
+                      )
+                    })}
+                  </ItemsCarousel>
                 )
-              })}
-            </ItemsCarousel>
+              }
+            </Media>
           </div>
         )
       }}
